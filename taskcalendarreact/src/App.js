7088ec7c0edddca9a2944a031a9e3a550e30d30f
iAmKevinCruz <kcruzroyalty@gmail.com>
2021-09-04 18:27:24 -0400

    updated the important icon

--- taskcalendarreact/src/App.js
+++ taskcalendarreact/src/App.js
@@ -1,18 +1,27 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { library } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core";
-import { fas} from '@fortawesome/free-solid-svg-icons';
-import { far} from '@fortawesome/free-regular-svg-icons';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import TaskInput from './components/taskInput';
+import "./App.css";
+import TaskInput from "./components/taskInput";
+import React, { useState } from "react";
 
 library.add(fas, far);
 
-
 function App() {
+  const [important, setImportant] = useState(false);
+
+  const handleImportant = (imp) => {
+    console.log(`Change Importance to ${imp}`);
+    setImportant(imp);
+  };
+
+  console.log(important);
+
   return (
     <div className="">
-      <TaskInput></TaskInput>
+      <TaskInput onChange={handleImportant}></TaskInput>
     </div>
   );
 }
